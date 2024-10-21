1a89ad418d4a7c7ad07ee184ef8da2a9f4fd3dd0
mbonikevin <k.mbonimpay@alustudent.com>
2024-10-21 14:53:45 +0200

    fixed

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123): 
-    if chr(i) != 'e' and chr(i) != 'q': print(chr(i), end="")
-
+for letter in range(97, 123):
+    if chr(letter) != 'e' and chr(letter) != 'q':
+        print("{}".format(chr(letter)), end="")
