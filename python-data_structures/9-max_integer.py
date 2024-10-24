62b8cbf0fcb37b71f037760ca517a9e5ff75456f
mbonikevin <k.mbonimpay@alustudent.com>
2024-10-24 10:11:57 +0200

    (8 & 9) 2 empty lines added

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def max_integer(my_list=[]):
     if not my_list:
         return None
@@ -8,5 +9,5 @@ def max_integer(my_list=[]):
     for num in my_list:
         if num > max_value:
             max_value = num
-            
+
     return max_value
