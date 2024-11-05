48a08c9fea6e6459cf8e958bf4b2e9710fdb677f
mbonikevin <k.mbonimpay@alustudent.com>
2024-11-05 08:28:33 +0200

    6 lines too long fix M8 validation

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""this module defins a square class"""
+"""This module defines a square class."""
+
 
 class Square:
-    """squar class with size and aria."""
+    """Square class with size and position."""
+
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
@@ -25,7 +27,9 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) for i in value) or not all(i >= 0 for i in value):
+        if not isinstance(value, tuple) or len(value) != 2 or not all(
+            isinstance(i, int) for i in value
+        ) or not all(i >= 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
