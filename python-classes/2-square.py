347529cfcca125a525aca3623b5837ebdcb7c086
mbonikevin <k.mbonimpay@alustudent.com>
2024-11-05 07:47:35 +0200

    2

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Class Square that defines a square with a private attribute size and validation."""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
