f7159ff80d1398b69f17a1fe41c7ecd1e8288ec9
mbonikevin <k.mbonimpay@alustudent.com>
2024-11-05 08:13:25 +0200

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this module defins a squar clas"""
+
+
+class Square:
+    """defines a squar with a siez"""
+
+    def __init__(self, size):
+        """initializes the siez of the squar"""
+        self.__size = size
