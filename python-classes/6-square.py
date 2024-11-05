48b80d61917eec0d1146358dfc8bdbd8b3bdd7f8
mbonikevin <k.mbonimpay@alustudent.com>
2024-11-05 08:13:43 +0200

    6

---
+++ python-classes/6-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""this module defins a square class"""
+
+class Square:
+    """Class Square that defines a square with a private attribute size, validation, area calculation, position management, and printing functionality."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) for i in value) or not all(i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square using the '#' character, positioned as specified."""
+        if self.__size == 0:
+            print()
+        else:
+            print("\n" * self.__position[1], end='')
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
