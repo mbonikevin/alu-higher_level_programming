03602151dbb149bef7a3c0e499bd735571fd26d6
mbonikevin <k.mbonimpay@alustudent.com>
2024-11-07 08:17:33 +0200

    3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -39,3 +39,11 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        return "\n".join(["#" * self.width] * self.height)
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
