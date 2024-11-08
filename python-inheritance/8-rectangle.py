d17decaafc8e46c018ac21c6ac4080d146c4551c
mbonikevin <k.mbonimpay@alustudent.com>
2024-11-08 09:53:50 +0200

    8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -20,6 +20,10 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
+    def area(self):
+        """calculates the area of the rectangle"""
+        return self.__width * self.__height
+
     def __str__(self):
         """returns the string representation of the rectangle"""
         return f"[Rectangle] {self.__width}/{self.__height}"
