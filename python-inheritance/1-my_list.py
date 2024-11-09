f5c2ba782cf3f918056666c6009fcbfe31612667
mbonikevin <k.mbonimpay@alustudent.com>
2024-11-09 20:13:04 +0200

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,6 +4,7 @@
 
 class MyList(list):
     """subclass of list with a method to print the sorted list"""
+
     def print_sorted(self):
         """printnig the list in ascending order"""
         print(sorted(self))
