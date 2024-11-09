0ffa14d0ae8afe9b5337ed681a1beb0eaed34d6c
mbonikevin <k.mbonimpay@alustudent.com>
2024-11-09 20:20:11 +0200

    8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,29 +2,20 @@
 """rectangle class that inherits from basegeometry"""
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+
 class BaseGeometry:
     """basegeometry class with integer_validator method"""
-    def integer_validator(self, name, value):
-        """validates the value as a positive integer"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """rectangle class that inherits from basegeometry"""
     def __init__(self, width, height):
-        """initializes the width and height, both are validated"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """seting with width and height"""
+        super().integer_validator("width", width)
         self.__width = width
+        super().integer_validator("height", height)
         self.__height = height
 
-    def area(self):
-        """calculates the area of the rectangle"""
-        return self.__width * self.__height
 
-    def __str__(self):
-        """returns the string representation of the rectangle"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
+    def area(self):
+        """area methos"""
+        return self.__width * self.__height
