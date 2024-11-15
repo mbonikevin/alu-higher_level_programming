356c8048558dd0a9b98188cd8d9992edf9e62ae7
mbonikevin <k.mbonimpay@alustudent.com>
2024-11-15 10:07:43 +0200

    0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
-
--- 0. my privileges
-SELECT user, host, db, table_name, privilege_type
-FROM information_schema.role_table_grants
-WHERE grantee = "'user_0d_1'@'localhost'" OR grantee = "'user_0d_2'@'localhost';
+-- my privileges
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
