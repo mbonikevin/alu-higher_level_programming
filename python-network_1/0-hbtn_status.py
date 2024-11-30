c26c185ba159b3420eda13ae633b56e2eebc1ff5
mbonikevin <k.mbonimpay@alustudent.com>
2024-11-30 10:55:13 +0200

    error resolved 0 - 1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,11 +2,15 @@
 """ fetching https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = "https://alu-intranet.hbtn.io/status"
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    content = response.read()
-
-print("Body response:")
-print("\t- type: {}".format(type(content)))
-print("\t- content: {}".format(content))
-print("\t- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode("utf-8"))
--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """displaying value of the X-Request-Id from header"""
 import urllib.request
-from sys import argv
-
+import sys
 
 if __name__ == "__main__":
-    url = argv[1]
+    url = sys.argv[1]
 
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
