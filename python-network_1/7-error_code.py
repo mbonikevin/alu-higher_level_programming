a78998a21f3502a663558e2518323ee0d6faef3a
mbonikevin <k.mbonimpay@alustudent.com>
2024-11-30 11:17:23 +0200

    Whitespaces removed

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Script that takes in a URL, sends a request to the URL and displays the body of the response"""
+"""Script that takes in a URL, sends a request to
+the URL and displays the body of the response"""
 import sys
 import requests
 
