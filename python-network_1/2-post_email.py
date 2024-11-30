2ab5b1d077c506ec1762358e3adc55ebe634c539
mbonikevin <k.mbonimpay@alustudent.com>
2024-11-30 10:58:42 +0200

    empty endlines and 2

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -2,13 +2,14 @@
 """sending a POST request to the givven URL"""
 import urllib.request
 import urllib.parse
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
-    url = argv[1]
-    email = argv[2]
+    url = sys.argv[1]
+    email = sys.argv[2]
     content = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
     req = urllib.request.Request(url, data=content, method='POST')
     with urllib.request.urlopen(req) as response:
         content = response.read().decode('utf-8')
