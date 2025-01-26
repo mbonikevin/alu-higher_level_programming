93a669e8e8da63e4a144daf4582b60f2948232e9
mbonikevin <k.mbonimpay@alustudent.com>
2025-01-26 06:29:35 -0800

    4 error

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-console.log(process.argv[2] + ' is ' + process.argv[3] === undefined ? 'undefined' : process.argv[3]);
+console.log(process.argv[2] + ' is ' + (process.argv[3] === undefined ? 'undefined' : process.argv[3]));
