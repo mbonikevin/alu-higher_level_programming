fa5123b66d1a9e0cab20055ede85405c98c23a8c
mbonikevin <k.mbonimpay@alustudent.com>
2025-02-11 08:29:33 +0200

    scrapping

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const numberOfCompletedTasks = () => {
+  const url = process.argv[2];
+  const request = require('request');
+  const userIdList = [];
+  const completed = {};
+
+  request.get(url, (error, response, body) => {
+    if (error) throw error;
+    const data = JSON.parse(body);
+    data.forEach((item, index) => {
+      if (!userIdList.includes(item.userId) && item.completed === true) {
+        userIdList.push(item.userId);
+        completed[item.userId] = 0;
+      }
+      if (item.completed) {
+        completed[item.userId]++;
+      }
+    });
+    
+    console.log(completed);
+  });
+};
+numberOfCompletedTasks();
